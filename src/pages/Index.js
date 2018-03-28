cf6a55cfcab3417c0b51d6ec31cb5a6a138efc55
makebanana <530547479@qq.com>
2018-03-29 01:16:13 +0800

    [coding] main container

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledAppWrap = styled.div`
+  width: 100%;
+  min-width: 800px;
+  height: 100vh;
+`;
+
+const StyledHeader = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #404040;
+`;
+
+const StyledContain = styled.div`
+  display: flex;
+  height: calc(100vh - 60px);
+`;
+
+const StyledLeftNav = styled.div`
+  width: 200px;
+  height: 100%;
+  background-color: #404040;
+`;
+
+const StyledMain = styled.div`
+  width: 100%;
+`;
 
 export default class Login extends Component {
   static contextTypes = {
@@ -7,6 +35,16 @@ export default class Login extends Component {
   }
 
   render() {
-    return <div>123</div>;
+    return (
+      <StyledAppWrap>
+        <StyledHeader>
+          123
+        </StyledHeader>
+        <StyledContain>
+          <StyledLeftNav>12322</StyledLeftNav>
+          <StyledMain>312312</StyledMain>
+        </StyledContain>
+      </StyledAppWrap>
+    );
   }
 }
